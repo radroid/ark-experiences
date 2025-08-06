c5af718de14c1317b291f8ca112a042a7db6be77
Raj D <25481060+radroid@users.noreply.github.com>
2025-08-06 11:27:28 -0400

    feat: Add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/components/ui/navbar'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -50,6 +51,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
