0f212252aa8fb02eaec9e641b23b216bb4003ab7
Raj D <25481060+radroid@users.noreply.github.com>
2025-10-14 01:48:41 -0400

    fix: Remove unnecessary files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,11 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  {
-    ignores: [
-      "src/components/ui/splash-cursor.tsx",
-    ],
-  },
+  {},
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
